058d12326ee51d4ccfaeccf3c75dccf59255aa61
BeggarsPeterM <petermenniti@beggars.com>
2019-09-27 17:13:51 -0400

    unit tests for material model

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,10 +1,14 @@
 class Material < ApplicationRecord
 
   # associations
-  has_many :materials_produced, class_name: "Blueprint", foreign_key: :material_produced_id
-  has_many :materials_required, class_name: "Blueprint", foreign_key: :material_requried_id
+  has_many :blueprints, class_name: "Blueprint", foreign_key: :material_produced_id
+  has_many :blueprint_inclusions, class_name: "Blueprint", foreign_key: :material_required_id
+  has_many :materials_required, through: :blueprints, source: :material_required
+  has_many :materials_produced, through: :blueprint_inclusions, source: :material_produced
   has_many :byproducts
-  has_many :byproduct_of, class_name: "Byproduct", foreign_key: :byproduct_id
+  has_many :byproducts_of, class_name: "Byproduct", foreign_key: :byproduct_id
+  has_many :byproduct_materials, through: :byproducts, source: :byproduct
+  has_many :byproduct_of_materials, through: :byproducts_of, source: :material
 
   # validations
   validates :material_name, presence: true, uniqueness: true
@@ -24,8 +28,8 @@ class Material < ApplicationRecord
     !!inventory_spaces
   end
 
-  def crafted?
-    materials_required.empty?
+  def craftable?
+    !materials_required.empty?
   end
 
   def marine_growbed_spaces
@@ -40,21 +44,29 @@ class Material < ApplicationRecord
     growbed_spaces if material_type == "Fauna"
   end
 
-  def requires_indoor_space
+  def requires_indoor_space?
     material_type = "Base Piece" && ["Interior Module","Interior Piece","Miscellaneous"].include?(material_subtype)
   end
 
   private
 
   def crafted_item_has_location
-    if !materials_required.empty? && !crafted_at.present?
-      errors.add(:crafted_at, "crafted item must be crafted someplace")
+    if Blueprint.all.any? # skip it if they havent been seeded
+      if craftable? && !crafted_at.present?
+        errors.add(:crafted_at, "crafted item must be crafted someplace")
+      elsif !craftable? && crafted_at.present?
+        errors.add(:crafted_at, "non-craftable item cannot be crafted someplace")
+      end
     end
   end
 
   def crafted_item_produces_quantity
-    if !materials_required.empty? && !number_produced.present?
-      errors.add(:number_produced, "crafted item must produce a quantity")
+    if Blueprint.all.any? # skip it if they havent been seeded
+      if craftable? && !number_produced.present?
+        errors.add(:number_produced, "crafted item must produce a quantity")
+      elsif !craftable? && number_produced.present?
+        errors.add(:number_produced, "non-craftable item cannot be produced")
+      end
     end
   end
 end
--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -1,5 +1,269 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Material, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @material = Material.create({
+      material_name: "Test Material I",
+      material_type: "Test Type"
+    })
+  end
+
+  # validations
+  it "is valid so long as it has both a name and a type" do
+    expect(@material.valid?).to eq(true)
+  end
+
+  it "is invalid without a name" do
+    @material.material_name = nil
+    @material.valid?
+    expect(@material.errors[:material_name]).not_to eq([])
+  end
+
+  it "is invalid with a duplicate name" do
+    @material2 = Material.create({
+      material_name: "Test Material I",
+      material_type: "Test Type"
+    })
+    @material2.valid?
+    expect(@material2.errors[:material_name]).not_to eq([])
+  end
+
+  it "is invalid without a type" do
+    @material.material_type = nil
+    @material.valid?
+    expect(@material.errors[:material_type]).not_to eq([])
+  end
+
+  it "is valid with a positive number of inventory spaces" do
+    @material.inventory_spaces = 1
+    expect(@material.valid?).to eq(true)
+  end
+
+  it "is invalid with 0 inventory spaces" do
+    @material.inventory_spaces = 0
+    @material.valid?
+    expect(@material.errors[:inventory_spaces]).not_to eq([])
+  end
+
+  it "is invalid with a negative number of inventory spaces" do
+    @material.inventory_spaces = -1
+    @material.valid?
+    expect(@material.errors[:inventory_spaces]).not_to eq([])
+  end
+
+  it "is valid with a positive number of growbed spaces" do
+    @material.growbed_spaces = 1
+    expect(@material.valid?).to be true
+  end
+
+  it "is invalid with 0 growbed spaces" do
+    @material.growbed_spaces = 0
+    @material.valid?
+    expect(@material.errors[:growbed_spaces]).not_to eq([])
+  end
+
+  it "is invalid with a negative number of growbed spaces" do
+    @material.growbed_spaces = -1
+    @material.valid?
+    expect(@material.errors[:growbed_spaces]).not_to eq([])
+  end
+
+  it "is valid with a positive number of storage spaces provided" do
+    @material.storage_spaces_provided = 1
+    expect(@material.valid?).to eq(true)
+  end
+
+  it "is invalid with 0 storage spaces provided" do
+    @material.storage_spaces_provided = 0
+    @material.valid?
+    expect(@material.errors[:storage_spaces_provided]).not_to eq([])
+  end
+
+  it "is invalid with a negative number of storage spaces provided" do
+    @material.storage_spaces_provided = -1
+    @material.valid?
+    expect(@material.errors[:storage_spaces_provided]).not_to eq([])
+  end
+
+  # behaviors
+  it "is not craftable if it does not require materials" do
+    expect(@material.craftable?).to eq(false)
+  end
+
+  it "is not carryable if it does not list the inventory space it requires" do
+    expect(@material.carryable?).to eq(false)
+  end
+
+  it "is carryable if it takes up inventory space" do
+    @material.inventory_spaces = 1
+    expect(@material.carryable?).to eq(true)
+  end
+
+  it "returns marine growbed spaces required if it is Marine Flora" do
+    @material.material_type = "Flora"
+    @material.material_subtype = "Marine"
+    @material.growbed_spaces = 1
+    expect(@material.marine_growbed_spaces).to eq(1)
+  end
+
+  it "returns land growbed spaces required if it is Land Flora" do
+    @material.material_type = "Flora"
+    @material.material_subtype = "Land"
+    @material.growbed_spaces = 1
+    expect(@material.land_growbed_spaces).to eq(1)
+  end
+
+  it "returns containment spaces required if it is Fauna" do
+    @material.material_type = "Fauna"
+    @material.growbed_spaces = 1
+    expect(@material.containment_spaces).to eq(1)
+  end
+
+  it "requires indoor space if it is an Interior Module" do
+    @material.material_type = "Base Piece"
+    @material.material_subtype = "Interior Module"
+    expect(@material.requires_indoor_space?).to eq(true)
+  end
+
+  it "requires indoor space if it is an Interior Piece" do
+    @material.material_type = "Base Piece"
+    @material.material_subtype = "Interior Piece"
+    expect(@material.requires_indoor_space?).to eq(true)
+  end
+
+  it "requires indoor space if it is a Miscellaneous Base Piece" do
+    @material.material_type = "Base Piece"
+    @material.material_subtype = "Miscellaneous"
+    expect(@material.requires_indoor_space?).to eq(true)
+  end
+
+  it "does not require indoor space if it is not an Interior Piece, Interior Module or Miscellaneous Base Piece" do
+    expect(@material.requires_indoor_space?).to eq(false)
+  end
+
+  it "does not return marine growbed spaces required if it is not Marine Flora" do
+    @material.growbed_spaces = 1
+    expect(@material.marine_growbed_spaces).to eq(nil)
+  end
+
+  it "does not return land growbed spaces required if it is not Land Flora" do
+    @material.growbed_spaces = 1
+    expect(@material.land_growbed_spaces).to eq(nil)
+  end
+
+  it "does not return containment spaces required if it is not Fauna" do
+    @material.growbed_spaces = 1
+    expect(@material.containment_spaces).to eq(nil)
+  end
+
+  context "with a blueprint" do
+    before(:each) do
+      @material2 = Material.create({
+        material_name: "Test Material II",
+        material_type: "Test Type"
+      })
+
+      @blueprint = Blueprint.create({
+        material_produced: @material,
+        material_required: @material2,
+        number_required: 1
+      })
+    end
+
+    # validations
+    it "is invalid with a number produced if it can't be crafted" do
+      @material2.number_produced = 1
+      @material2.valid?
+      expect(@material2.errors[:number_produced]).not_to eq([])
+    end
+
+    it "is invalid with a crafting location if it can't be crafted" do
+      @material2.crafted_at = "Fabricated"
+      @material2.valid?
+      expect(@material2.errors[:crafted_at]).not_to eq([])
+    end
+
+    it "is invalid without a number produced if it can be crafted" do
+      @material.valid?
+      expect(@material.errors[:number_produced]).not_to eq([])
+    end
+
+    it "is valid if it can be crafted and produces a positive quantity" do
+      @material.number_produced = 1
+      @material.valid?
+      expect(@material.errors[:number_produced]).to eq([])
+    end
+
+    it "is invalid if it can be crafted and produces 0" do
+      @material.number_produced = 0
+      @material.valid?
+      expect(@material.errors[:number_produced]).not_to eq([])
+    end
+
+    it "is invalid if it can be crafted and produces a negative quantity" do
+      @material.number_produced = -1
+      @material.valid?
+      expect(@material.errors[:number_produced]).not_to eq([])
+    end
+
+    it "is invalid if it can be crafted and does not have a crafting location" do
+      @material.valid?
+      expect(@material.errors[:crafted_at]).not_to eq([])
+    end
+
+    it "is valid if it has a crafting location and can be crafted" do
+      @material.crafted_at = "Fabricated"
+      @material.valid?
+      expect(@material.errors[:crafted_at]).to eq([])
+    end
+
+    # associations
+    it "returns blueprints where it is the material being produced" do
+      expect(@material.blueprints).to include(@blueprint)
+    end
+
+    it "returns blueprints where it is the material being worked" do
+      expect(@material2.blueprint_inclusions).to include(@blueprint)
+    end
+
+    it "returns the products used to produce it" do
+      expect(@material.materials_required).to include(@material2)
+    end
+
+    it "returns the products it can be used to produce" do
+      expect(@material2.materials_produced).to include(@material)
+    end
+  end
+
+  context "with a byproduct" do
+    before(:each) do
+      @material2 = Material.create({
+        material_name: "Test Material II",
+        material_type: "Test Type"
+      })
+
+      @byproduct = Byproduct.create({
+        material: @material,
+        byproduct: @material2,
+        number_produced: 1
+      })
+    end
+
+    # associations
+    it "returns instances of itself producing byproducts" do
+      expect(@material.byproducts).to include(@byproduct)
+    end
+
+    it "returns instances where crafting an item produces it as a byproduct" do
+      expect(@material2.byproducts_of).to include(@byproduct)
+    end
+
+    it "returns materials it produces as byproducts" do
+      expect(@material.byproduct_materials).to include(@material2)
+    end
+
+    it "returns materials that produce it as a byproduct" do
+      expect(@material2.byproduct_of_materials).to include(@material)
+    end
+  end
 end
