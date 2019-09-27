3cb9f5a90d3bb34d9cd4ba03690e7673b335db96
BeggarsPeterM <petermenniti@beggars.com>
2019-09-27 18:01:26 -0400

    blueprint and byproduct unit tests

--- spec/models/blueprint_spec.rb
+++ spec/models/blueprint_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Blueprint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @material = Material.create({
+      material_name: "The Finished Good",
+      material_type: "Test Type"
+    })
+
+    @material2 = Material.create({
+      material_name: "The Raw Material",
+      material_type: "Test Type"
+    })
+
+    @blueprint = Blueprint.create({
+      material_produced: @material,
+      material_required: @material2,
+      number_required: 1
+    })
+  end
+
+  # validations
+  it "is valid with a material produced, material required and a positive number required" do
+    expect(@blueprint.valid?).to eq(true)
+  end
+
+  it "is invalid without a number of materials required" do
+    @blueprint.number_required = nil
+    @blueprint.valid?
+    expect(@blueprint.errors[:number_required]).not_to eq([])
+  end
+
+  it "is invalid without a material produced" do
+    @blueprint.material_produced = nil
+    @blueprint.valid?
+    expect(@blueprint.errors[:material_produced]).not_to eq([])
+  end
+
+  it "is invalid without a material required" do
+    @blueprint.material_required = nil
+    @blueprint.valid?
+    expect(@blueprint.errors[:material_required]).not_to eq([])
+  end
+
+  it "is invalid if it requires 0 materials" do
+    @blueprint.number_required = 0
+    @blueprint.valid?
+    expect(@blueprint.errors[:number_required]).not_to eq([])
+  end
+
+  it "is invalid if it requires a negative number of materials" do
+    @blueprint.number_required = -1
+    @blueprint.valid?
+    expect(@blueprint.errors[:number_required]).not_to eq([])
+  end
+
+  # associations
+  it "returns the material it is meant to craft" do
+    expect(@blueprint.material_produced).to equal(@material)
+  end
+
+  it "returns the material it requires" do
+    expect(@blueprint.material_required).to equal(@material2)
+  end
 end
--- spec/models/byproduct_spec.rb
+++ spec/models/byproduct_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Byproduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @material = Material.create({
+      material_name: "The Finished Good",
+      material_type: "Test Type"
+    })
+
+    @material2 = Material.create({
+      material_name: "The Byproduct",
+      material_type: "Test Type"
+    })
+
+    @byproduct = Byproduct.create({
+      material: @material,
+      byproduct: @material2,
+      number_produced: 1
+    })
+  end
+
+  # validations
+  it "is valid with a material, byproduct and number produced" do
+    expect(@byproduct.valid?).to eq(true)
+  end
+
+  it "is not valid without a material producing the byproduct" do
+    @byproduct.material = nil
+    @byproduct.valid?
+    expect(@byproduct.errors[:material]).not_to eq([])
+  end
+
+  it "is not valid without a byproduct being produced" do
+    @byproduct.byproduct = nil
+    @byproduct.valid?
+    expect(@byproduct.errors[:byproduct]).not_to eq([])
+  end
+
+  it "is not valid without a number being produced" do
+    @byproduct.number_produced = nil
+    @byproduct.valid?
+    expect(@byproduct.errors[:number_produced]).not_to eq([])
+  end
+
+  it "is not valid if it produces 0 byproduct" do
+    @byproduct.number_produced = 0
+    @byproduct.valid?
+    expect(@byproduct.errors[:number_produced]).not_to eq([])
+  end
+
+  it "is not valid if it produces a negative quantity of byproduct" do
+    @byproduct.number_produced = -1
+    @byproduct.valid?
+    expect(@byproduct.errors[:number_produced]).not_to eq([])
+  end
+
+  # associations
+  it "returns the material producing the byproduct" do
+    expect(@byproduct.material).to equal(@material)
+  end
+
+  it "returns the material being produced as byproduct" do
+    expect(@byproduct.byproduct).to equal(@material2)
+  end
 end
