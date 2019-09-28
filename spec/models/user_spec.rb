a90371680909bc2b8236073207839258536aa28b
BeggarsPeterM <petermenniti@beggars.com>
2019-09-28 12:53:09 -0400

    a few unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+  	@user = User.create(email: "email@emails.com", password: "abcd1234")
+  end
+
+  it "is valid so long as it has an email and a password" do
+  	expect(@user.valid?).to eq(true)
+  end
+
+  it "is invalid without an email" do
+  	@user.email = nil
+  	@user.valid?
+  	expect(@user.errors[:email]).not_to eq([])
+  end
+
+  it "is invalid with a duplicate email" do
+  	@user2 = User.create(email: "email@emails.com", password: "qwertyiou")
+  	@user2.valid?
+  	expect(@user2.errors[:email]).not_to eq([])
+  end
 end
