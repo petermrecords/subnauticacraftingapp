e493b59b8ff73156bd67523fbf0385c3409b3017
BeggarsPeterM <petermenniti@beggars.com>
2019-10-03 13:46:46 -0400

    bootstrap the rest of the user forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def validated_form_class(resource,attribute)
+    if resource.errors[attribute].any?
+      "form-control is-invalid"
+    elsif resource.errors.any?
+      "form-control is-valid"
+    else
+      "form-control"
+    end
+  end
+
+  def error_for(resource, attribute, help_text=nil)
+    if resource.errors[attribute]
+      render partial: "form_error_message", locals: { resource: resource, attribute: attribute, help_text: help_text }
+    end
+  end
 end
